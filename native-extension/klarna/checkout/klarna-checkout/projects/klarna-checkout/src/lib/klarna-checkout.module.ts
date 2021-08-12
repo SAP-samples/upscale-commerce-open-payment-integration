5062c9b6d7b55ad02263306a75dcdee1a3d1d5ff
I854087 <adriaan.venter@sap.com>
2021-08-12 15:30:44 -0400

    sample klarna checkout component

---
+++ native-extension/klarna/checkout/klarna-checkout/projects/klarna-checkout/src/lib/klarna-checkout.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RegistrationService } from 'upscale-web-storefront-sdk';
+import { KlarnaCheckoutComponent } from './klarna-checkout.component';
+
+@NgModule({
+  declarations: [KlarnaCheckoutComponent],
+  exports: [KlarnaCheckoutComponent],
+})
+export class KlarnaCheckoutModule {
+  constructor(private registrationService: RegistrationService) {
+    this.registrationService.register(
+      'klarna-checkout',
+      KlarnaCheckoutComponent
+    );
+  }
+}
---
+++ native-extension/klarna/checkout/klarna-checkout/projects/klarna-checkout/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of klarna-checkout
+ */
+
+export * from './lib/klarna-checkout.service';
+export * from './lib/klarna-checkout.component';
+export * from './lib/klarna-checkout.module';
